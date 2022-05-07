afbdff87ca3b1293fb1264493f4994661b91c39b
Kory Smith <kor54e@gmail.com>
2022-05-07 14:34:42 -0400

    scaffold action function

--- app/routes/webhook/peat.tsx
+++ app/routes/webhook/peat.tsx
@@ -5,3 +5,26 @@ export const loader: LoaderFunction = async ({ request }) => {
   console.log({ request })
 	return json({name: "kory"})
 }
+
+export const action: ActionFunction = async ({request}) => {
+  if (request.method !== "POST") {
+    return json({ message: "Method not allowed" }, 405);
+  }
+  const payload = await request.json();
+
+	  /* Validate the webhook */
+		const signature = request.headers.get(
+			"X-Hub-Signature-256"
+		);
+		if (process.env.GITHUB_WEBHOOK_SECRET) {
+			const generatedSignature = `sha256=${crypto
+				.createHmac("sha256", process.env.GITHUB_WEBHOOK_SECRET)
+				.update(JSON.stringify(payload))
+				.digest("hex")}`;
+			if (signature !== generatedSignature) {
+				return json({ message: "Signature mismatch" }, 401);
+			}
+			// Do webhook stuff here
+			return json({success: true}, 200)
+		}
+}
