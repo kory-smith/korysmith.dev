64ebd155fd656baefdfc5213a347f135997d9fe4
Kory Smith <kor54e@gmail.com>
2022-05-24 21:06:36 -0400

    change default seo values for me

--- app/utils/seo.ts
+++ app/utils/seo.ts
@@ -1,13 +1,13 @@
 import { initSeo } from 'remix-seo'
 
 export const { getSeo, getSeoLinks, getSeoMeta } = initSeo({
-  title: 'Remix Blog',
-  description: 'Blog built using Remix',
+  title: 'KorySmith.dev',
+  description: "Kory Smith's development playground and portfolio site",
   twitter: {
     card: 'summary',
-    creator: '@handle',
-    site: 'https://my-site.dev',
-    title: 'Remix Blog',
-    description: 'Blog built using Remix',
+    creator: '@Kor54e',
+    site: 'https://korysmith.dev/',
+    title: 'KorySmith.dev',
+    description: "Kory Smith's development playground and portfolio site",
   },
 })
