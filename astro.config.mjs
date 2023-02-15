125713df2760896aee588f4fdea7db55e2bf293c
Kory Smith <kory.smith@kroger.com>
2023-02-15 09:59:22 -0500

    switches to cloudflare directory mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare()
+  output: "server",
+  adapter: cloudflare({ mode: "directory" }),
 });
