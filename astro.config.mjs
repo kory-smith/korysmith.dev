2fe7a19e2901997e79e4c18ab62da02528e39450
Kory Smith <kory.smith@kroger.com>
2023-02-15 11:06:10 -0500

    adds tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
-
 // https://astro.build/config
 import cloudflare from "@astrojs/cloudflare";
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare({ mode: "directory" }),
+  adapter: cloudflare({
+    mode: "directory",
+  }),
+  integrations: [tailwind()],
 });
