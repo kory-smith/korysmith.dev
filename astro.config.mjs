1800076a79cc316a0a939b9eae1f309c3a9b0003
Kory Smith <kory.smith@kroger.com>
2023-03-06 11:14:58 -0500

    adds prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 // https://astro.build/config
 import cloudflare from "@astrojs/cloudflare";
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +12,10 @@ export default defineConfig({
   }),
   integrations: [
     tailwind({
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
+    prefetch({ throttle: 3 }),
   ],
 });
