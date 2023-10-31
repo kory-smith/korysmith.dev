e8d4647e39b6188c4d4b3b77ff7cfe7ca9d1ec9d
Kory Smith <kory.smith@kroger.com>
2023-10-31 14:41:28 -0400

    autoformat

--- src/scripts/fetchArticles.js
+++ src/scripts/fetchArticles.js
@@ -1,11 +1,11 @@
-import fetch from 'node-fetch';
-import imageData from "~/public/images/imageData.json"
+import fetch from "node-fetch";
+import imageData from "~/public/images/imageData.json";
 
-const NOTION_SECRET = process.env['PUBLIC_NOTION_SECRET']
-const ARTICLES_DATABASE_ID = 'bf7e16c44b7b46a6ac4d11d5d4db77d8';
+const NOTION_SECRET = process.env["PUBLIC_NOTION_SECRET"];
+const ARTICLES_DATABASE_ID = "bf7e16c44b7b46a6ac4d11d5d4db77d8";
 
 function handleImage(result) {
-  const attributes = generateAttributes(result)
+  const attributes = generateAttributes(result);
   return `<picture>
             <source srcset="/images/${result.id}.avif" type="image/avif" ${attributes}>
             <source srcset="/images/${result.id}.webp" type="image/webp" ${attributes}>
@@ -21,11 +21,11 @@ function generateAttributes(result) {
     loading: "lazy",
     decoding: "async",
     // alt: result.image.caption[0].plain_text || "",
-  }
+  };
 
   return Object.entries(attributes).reduce((acc, [key, value]) => {
-    return `${acc} ${key}="${value}"`
-  }, "")
+    return `${acc} ${key}="${value}"`;
+  }, "");
 }
 
 function notionRichTextToHtml(richText) {
@@ -71,21 +71,30 @@ async function fetchChildBlocksRecursively(blockId) {
     }
   ).then((res) => res.json());
 
-  const list = []
+  const list = [];
   for (const childBlock of childBlocks) {
     list.push({
-      children: childBlock.has_children ? await fetchChildBlocksRecursively(childBlock.id) : null,
+      children: childBlock.has_children
+        ? await fetchChildBlocksRecursively(childBlock.id)
+        : null,
       type: childBlock.type,
       headerHelper: childBlock[childBlock.type]?.rich_text?.[0]?.plain_text,
       richText: childBlock[childBlock.type].rich_text,
-      id: childBlock.id
-    }) 
+      id: childBlock.id,
+    });
   }
-  return list
+  return list;
 }
 
 function isHeading(block) {
-  return ["heading_1", "heading_2", "heading_3", "heading_4", "heading_5", "heading_6"].includes(block.type);
+  return [
+    "heading_1",
+    "heading_2",
+    "heading_3",
+    "heading_4",
+    "heading_5",
+    "heading_6",
+  ].includes(block.type);
 }
 
 /**
@@ -116,8 +125,10 @@ const contentToHTML = (content) => {
       }
       const tag = createTag(block);
       let innerHTML = notionRichTextToHtml(block.richText);
-      innerHTML += `<a href="#${block.headerHelper.toLowerCase()}" class="anchor">🔗</a>`
-      html += wrapInTag(tag, innerHTML, [`id="${block.headerHelper.toLowerCase()}"`]);
+      innerHTML += `<a href="#${block.headerHelper.toLowerCase()}" class="anchor">🔗</a>`;
+      html += wrapInTag(tag, innerHTML, [
+        `id="${block.headerHelper.toLowerCase()}"`,
+      ]);
     } else if (block.type === "bulleted_list_item") {
       // If this is the first list item, start a new list and signal for later that we should close it
       if (!listHelper) {
@@ -175,26 +186,25 @@ const contentToHTML = (content) => {
 
 function createTag(block) {
   if (block.type === "paragraph") {
-    return "p" 
+    return "p";
   } else if (block.type === "heading_1") {
-    return "h1"
+    return "h1";
   } else if (block.type === "heading_2") {
-    return "h2"
+    return "h2";
   } else if (block.type === "heading_3") {
-    return "h3"
+    return "h3";
   } else if (block.type === "bulleted_list_item") {
-    return "li"
+    return "li";
   } else if (block.type === "numbered_list_item") {
-    return "li"
+    return "li";
   } else if (block.type === "divider") {
-    return "hr"
+    return "hr";
   } else if (block.type === "code") {
-    return "code"
+    return "code";
   } else if (block.type === "quote") {
-    return "blockquote"
-  }
-   else {
-    return ""
+    return "blockquote";
+  } else {
+    return "";
   }
 }
 
@@ -203,20 +213,23 @@ function wrapInTag(tag, content, attributes = []) {
 }
 
 export async function fetchArticlesUsingCustomFilter(filter) {
-  const response = await fetch(`https://api.notion.com/v1/databases/${ARTICLES_DATABASE_ID}/query`, {
-    method: 'POST',
-    // Don't include a body if there's no filter
-    ...(filter && { body: JSON.stringify(filter) }),
-    headers: {
-      'Content-Type': "application/json",
-      Authorization: `Bearer ${NOTION_SECRET}`,
-      'Notion-Version': '2022-06-28',
-    },
-  }).then((res) => res.json());
+  const response = await fetch(
+    `https://api.notion.com/v1/databases/${ARTICLES_DATABASE_ID}/query`,
+    {
+      method: "POST",
+      // Don't include a body if there's no filter
+      ...(filter && { body: JSON.stringify(filter) }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${NOTION_SECRET}`,
+        "Notion-Version": "2022-06-28",
+      },
+    }
+  ).then((res) => res.json());
 
   const articles = response.results.map(async (article) => {
     const content = await fetchChildBlocksRecursively(article.id);
-    const parsedContent = contentToHTML(content)
+    const parsedContent = contentToHTML(content);
     return {
       id: article.id,
       title: article.properties.Name.title[0].plain_text,
@@ -239,10 +252,10 @@ export async function fetchArticlesWithName(name) {
     filter: {
       property: "Name",
       rich_text: {
-        equals: name
-      }
-    }
-  })
+        equals: name,
+      },
+    },
+  });
 }
 
 export async function fetchArticlesWithStatus(status) {
@@ -250,8 +263,8 @@ export async function fetchArticlesWithStatus(status) {
     filter: {
       property: "Status",
       select: {
-        equals: status
-      }
-    }
-  })
+        equals: status,
+      },
+    },
+  });
 }
