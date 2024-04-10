a349480014ce1c4b9a5e283c2caf9345a1820a1b
Kory Smith <kory.smith@kroger.com>
2024-04-10 15:34:35 -0400

    updates blog config to enforce that if an article is published, it has a publishedDate

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,26 +1,43 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 const blogCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-		publishedDate: z.date(),
-		description: z.string(),
-    status: z.union([z.literal('published'), z.literal('draft')]),
-  }),
+  type: "content",
+  schema: z
+    .object({
+      title: z.string(),
+      publishedDate: z.date().optional(),
+      description: z.string(),
+      status: z.union([
+        z.literal("published"),
+        z.literal("draft"),
+        z.literal("idea"),
+      ]),
+    })
+    .refine(
+      (data) => {
+        if (data.status === "published" && !data.publishedDate) {
+          return false; // If status is 'published', publishedDate must exist
+        }
+        return true;
+      },
+      {
+        message: "Invalid combination of status and publishedDate",
+        path: ["status", "publishedDate"],
+      }
+    ),
 });
 
 // Pages like about, things-i-use, etc.
 const standaloneCollection = defineCollection({
-  type: 'content', // v2.5.0 and later
+  type: "content",
   schema: z.object({
     title: z.string(),
-		publishedDate: z.date(),
-		description: z.string(),
-  }),  
-})
+    publishedDate: z.date(),
+    description: z.string(),
+  }),
+});
 
 export const collections = {
-  'blog': blogCollection,
-  'standalone': standaloneCollection,
+  blog: blogCollection,
+  standalone: standaloneCollection,
 };
