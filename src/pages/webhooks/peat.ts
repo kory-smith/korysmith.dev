db131a9c32656a06491c2ca7db1f84fff13521f6
Kory Smith <kory.smith@kroger.com>
2025-01-27 15:50:49 -0500

    breaks out the todoist hmac generator into its own function

--- src/pages/webhooks/peat.ts
+++ src/pages/webhooks/peat.ts
@@ -16,22 +16,13 @@ export async function POST({
   const todoistSecret = locals.runtime.env.TODOIST_CLIENT_SECRET;
   if (todoistSecret) {
     const payload = await request.text();
-    const expectedHmac = request.headers.get("x-todoist-hmac-sha256");
 
-    const encoder = new TextEncoder();
-    const data = encoder.encode(payload);
-    const key = await crypto.subtle.importKey(
-      "raw",
-      encoder.encode(todoistSecret),
-      { name: "HMAC", hash: "SHA-256" },
-      false,
-      ["sign"]
-    );
-    const hmac = await crypto.subtle.sign("HMAC", key, data);
-    const generatedHmac = btoa(String.fromCharCode(...new Uint8Array(hmac)));
+    const expectedHmac = request.headers.get("x-todoist-hmac-sha256");
+    const generatedHmac = await generateTodoistHmac({ payload, todoistSecret });
     if (generatedHmac !== expectedHmac) {
       return new Response("Signature mismatch", { status: 401 });
     }
+
     const peatDispatch = await fetch(
       "https://api.github.com/repos/kory-smith/peat/dispatches",
       {
@@ -56,4 +47,24 @@ export async function POST({
   } else {
     return new Response("Todoist secret not set", { status: 500 });
   }
+}
+
+async function generateTodoistHmac({
+  payload,
+  todoistSecret,
+}: {
+  payload: string;
+  todoistSecret: string;
+}) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(payload);
+  const key = await crypto.subtle.importKey(
+    "raw",
+    encoder.encode(todoistSecret),
+    { name: "HMAC", hash: "SHA-256" },
+    false,
+    ["sign"]
+  );
+  const hmac = await crypto.subtle.sign("HMAC", key, data);
+  return btoa(String.fromCharCode(...new Uint8Array(hmac)));
 }
